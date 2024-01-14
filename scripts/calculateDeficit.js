167d9331456bc6f99bdca04c557340ccc5806bc5
Hollie Clarke <138706498+holdoescode@users.noreply.github.com>
2024-01-14 11:59:31 +0000

    Added animation for main text on home page to pulse

---
+++ scripts/calculateDeficit.js
@@ -0,0 +1,11 @@
+let form = document.getElementById('calorieDeficitForm');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+function calculateDeficit() {
+
+}
+
+console.log('the JS file is running');
