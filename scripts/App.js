5d3efdc127efd306433b0f1e3f889261c606ad15
Hollie Clarke <138706498+holdoescode@users.noreply.github.com>
2024-02-15 21:25:48 +0000

    basic react app set up on home page, but nothing renders yet

---
+++ scripts/App.js
@@ -0,0 +1,10 @@
+// import stylesheet
+// make sure jsx is transpiling and rendering properly 
+import React, { useState } from 'react';
+
+export default function App() {
+
+    return (
+
+    );
+}
